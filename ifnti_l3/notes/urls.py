58748410e2a9b961f11a78a26d650c8bcf792013
Arouna-Ramdane <ramdane.arouna@ifnti.com>
2025-10-06 08:00:06 +0000

    fin tp4

--- ifnti_l3/notes/urls.py
+++ ifnti_l3/notes/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
 from . import views
+from .views.eleve import *
+from .views.matiere import *   
+from .views.niveau import *   
+
+app_name="notes"
+
 urlpatterns = [
-	path('',views.index, name='index'),
+	path('', views.index, name='index'),
+    path('eleves/', views.eleves, name='eleves'),
+    path('eleve/<int:id>/', views.eleve, name='eleve'),
+    path('matieres/', views.matieres, name='matieres'),
+    path('matiere/<int:id>/', views.matiere, name='matiere'),
+    path('niveau/<int:id>/', views.niveau, name='niveau'),
 ]
